057aee25a90c198f6db88f87b1621f7ea59911ca
Nathan Lawrence <nathan@medium.com>
2015-08-25 10:56:05 -0700

    uvmon v2
    Big changes:
     - Enable/disable profiling for event loop ticks over 100ms
     - Log stats as a histogram
     - Use uv_now correctly and capture the entire tick duration.

--- uvmon.cc
+++ uvmon.cc
@@ -1,63 +1,51 @@
-#include <node.h>
-#include <v8.h>
-#include <uv.h>
-#include <string.h> /* memset */
 
 #include <nan.h>
+#include <node.h>
+#include <v8.h>
+#include <v8-profiler.h>
+#include <uv.h>
+
+#include <cstring>
+#include <iostream>
+
+#include "histogram.h"
+#include "profiler_wrapper.h"
 
 using namespace v8;
 
 static uv_check_t check_handle;
 
-typedef struct {
-  uint32_t count;
-  uint32_t sum_ms;
-  uint32_t slowest_ms;
-} mon_stats_t;
+static uint64_t last_ticked_at_ms;
 
-mon_stats_t uv_run_stats;
+static BucketingHistogram loop_stats(0, 1000, 10);
 
+static CpuProfilerWrapper profiler_wrapper(10);
 
-/* 
- * This is the callback that we register to be called at the end of 
+/**
+ * This is the callback that we register to be called at the end of
  * the uv_run() loop.
  */
-static void check_cb(uv_check_t* handle) {
-  if (handle == NULL) {
-    return;
-  }
-  if (handle->loop == NULL) {
-    return;
-  }
-  uv_loop_t* loop = handle->loop;
+static void check_run(uv_check_t* handle) {
+  uv_update_time(uv_default_loop());
+  uint64_t now_ms = uv_now(uv_default_loop());
+  uint64_t tick_delta_ms = now_ms - last_ticked_at_ms;
 
-#ifdef _WIN32
-  uv_loop_t tmp_loop;
-  memcpy(&tmp_loop, loop, sizeof(tmp_loop));
-  uv_update_time(&tmp_loop);
-  uint32_t now = tmp_loop.time;
-#else
-  uint32_t now = uv_hrtime() / 1000000;
-#endif
+  uint64_t profiling_delta_ms = profiler_wrapper.GetCurrentProfilingDuration();
+  if (profiler_wrapper.IsProfiling() && (profiling_delta_ms > 10000 || tick_delta_ms > 100)) {
+    v8::CpuProfile *profile = profiler_wrapper.Sample();
 
-  uint32_t delta;
+    if (tick_delta_ms > 100) {
+      profiler_wrapper.LogFilteredProfile(profile, now_ms, last_ticked_at_ms);
+    }
 
-  /* shouldn't have to check for this, but I swear I saw it happen once */
-  if (now < loop->time) {
-    delta = 0;
-  } else {
-    delta = (now - loop->time);
+    profile->Delete();
   }
 
-  uv_run_stats.count++;
-  uv_run_stats.sum_ms += delta;
-  if (delta > uv_run_stats.slowest_ms) {
-    uv_run_stats.slowest_ms = delta;
-  }
+  loop_stats.Sample(tick_delta_ms);
+  last_ticked_at_ms = now_ms;
 }
 
-
-/* 
+/**
  * Returns the data gathered from the uv_run() loop.
  * Resets all counters when called.
  */
@@ -65,41 +53,54 @@ NAN_METHOD(getData) {
   v8::Local<v8::Object> obj = Nan::New<v8::Object>();
 
   Nan::Set(obj, Nan::New<v8::String>("count").ToLocalChecked(),
-      Nan::New<v8::Number>(uv_run_stats.count));
-  Nan::Set(obj, Nan::New<v8::String>("sum_ms").ToLocalChecked(),
-      Nan::New<v8::Number>(uv_run_stats.sum_ms));
-  Nan::Set(obj, Nan::New<v8::String>("slowest_ms").ToLocalChecked(),
-      Nan::New<v8::Number>(uv_run_stats.slowest_ms));
-  memset(&uv_run_stats, 0, sizeof(uv_run_stats));
+      Nan::New<v8::Number>(loop_stats.GetSampleCount()));
+  Nan::Set(obj, Nan::New<v8::String>("p50_ms").ToLocalChecked(),
+      Nan::New<v8::Number>(loop_stats.GetApproximatePercentile(50)));
+  Nan::Set(obj, Nan::New<v8::String>("p95_ms").ToLocalChecked(),
+      Nan::New<v8::Number>(loop_stats.GetApproximatePercentile(95)));
+  Nan::Set(obj, Nan::New<v8::String>("p99_ms").ToLocalChecked(),
+      Nan::New<v8::Number>(loop_stats.GetApproximatePercentile(99)));
+  Nan::Set(obj, Nan::New<v8::String>("avg_ms").ToLocalChecked(),
+      Nan::New<v8::Number>(loop_stats.GetAverage()));
+  Nan::Set(obj, Nan::New<v8::String>("max_ms").ToLocalChecked(),
+      Nan::New<v8::Number>(loop_stats.GetMaximum()));
 
+  loop_stats.Reset();
   info.GetReturnValue().Set(obj);
 }
 
-
-/*
- * Stops the check callback.
- * Only used when loading multiple instances of uvmon (ie testing).
+/**
  */
-NAN_METHOD(stop) {
-  uv_check_stop(&check_handle);
-
+NAN_METHOD(stopProfiling) {
+  profiler_wrapper.Stop();
   info.GetReturnValue().SetUndefined();
 }
 
+/**
+ */
+NAN_METHOD(startProfiling) {
+  if (info.Length() != 1 || !info[0]->IsString()) {
+    return Nan::ThrowTypeError("Expected startProfiling(filename)");
+  }
 
-/*
+  profiler_wrapper.Start(*Nan::Utf8String(info[0]));
+  info.GetReturnValue().SetUndefined();
+}
+
+/**
  * Initialization and registration of methods with node.
  */
-void init(Handle<Object> target) {
-  memset(&uv_run_stats, 0, sizeof(uv_run_stats));
+void init(v8::Handle<v8::Object> target) {
+  last_ticked_at_ms = uv_now(uv_default_loop());
 
   /* set up uv_run callback */
   uv_check_init(uv_default_loop(), &check_handle);
   uv_unref(reinterpret_cast<uv_handle_t*>(&check_handle));
-  uv_check_start(&check_handle, check_cb);
+  uv_check_start(&check_handle, check_run);
 
   Nan::SetMethod(target, "getData", getData);
-  Nan::SetMethod(target, "stop", stop);
+  Nan::SetMethod(target, "startProfiling", startProfiling);
+  Nan::SetMethod(target, "stopProfiling", stopProfiling);
 }
 
 NODE_MODULE(uvmon, init);
